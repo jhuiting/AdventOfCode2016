5516cae5da0e302dd02efc8b4084062658f1886b
Jos Huiting <joshuiting4@gmail.com>
2016-12-15 18:21:01 +0100

    Refactored day 6, use function reference to DRY

--- src/day6/day6.kt
+++ src/day6/day6.kt
@@ -7,25 +7,15 @@ fun getLeastCommonCharacter(input: CharArray): Char = getLetterPairs(input).sort
 
 fun getLetterPairs(input: CharArray): List<Pair<Char, Int>> = input.groupBy(Char::toChar).mapValues { it.value.count() }.toList()
 
-private fun solvePartOne(allMessages: List<String>) {
-    val message = (0..allMessages[0].length - 1)
-            .map { i -> getMostCommonCharacter(allMessages.map { it[i] }.toCharArray()) }
+fun decodeMessage(allMessages: List<String>, charProcessor: (m: CharArray) -> Char): String {
+    return (0..allMessages[0].length - 1)
+            .map { i -> charProcessor(allMessages.map { it[i] }.toCharArray()) }
             .joinToString("")
-
-    println("6.1 - Decoded message is $message")
-}
-
-private fun solvePartTwo(allMessages: List<String>) {
-    val message = (0..allMessages[0].length - 1)
-            .map { i -> getLeastCommonCharacter(allMessages.map { it[i] }.toCharArray()) }
-            .joinToString("")
-
-    println("6.2 - Decoded message is $message")
 }
 
 fun main(args: Array<String>) {
     val allMessages = InputUtils().getInputAsText("src/day6/input.txt").readText().split("\n")
 
-    solvePartOne(allMessages)
-    solvePartTwo(allMessages)
+    println("6.1 - Decoded message is ${decodeMessage(allMessages, ::getMostCommonCharacter)}")
+    println("6.2 - Decoded message is ${decodeMessage(allMessages, ::getLeastCommonCharacter)}")
 }
