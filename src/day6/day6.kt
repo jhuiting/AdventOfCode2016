2dddcb489ce1d5403d66803c5634033e7ed76b2d
Jos Huiting <joshuiting4@gmail.com>
2016-12-15 10:01:34 +0100

    Refactored day 6, removed unused code

--- src/day6/day6.kt
+++ src/day6/day6.kt
@@ -2,47 +2,30 @@ package day6
 
 import util.InputUtils
 
-fun getLetterPairs(input: CharArray): List<Pair<Char, Int>> {
-    return input.filter(Char::isLetter)
-            .groupBy(Char::toChar)
-            .mapValues { it.value.count() }
-            .toList()
-}
+fun getMostCommonCharacter(input: CharArray): Char = getLetterPairs(input).sortedByDescending { it.second }[0].first
+fun getLeastCommonCharacter(input: CharArray): Char = getLetterPairs(input).sortedBy { it.second }[0].first
 
-fun getMostCommonCharacterList(input: CharArray): List<Char> {
-    return getLetterPairs(input)
-            .sortedByDescending { it.second }
-            .take(1)
-            .map {it.first}
-}
-
-fun getLeastCommonCharacterList(input: CharArray): List<Char> {
-    return getLetterPairs(input)
-            .sortedBy { it.second }
-            .take(1)
-            .map {it.first}
-}
-
-fun main(args: Array<String>) {
-    val allMessages = InputUtils().getInputAsText("src/day6/test-input.txt").readText().split("\n")
-
-    solvePartOne(allMessages)
-    solvePartTwo(allMessages)
-}
+fun getLetterPairs(input: CharArray): List<Pair<Char, Int>> = input.groupBy(Char::toChar).mapValues { it.value.count() }.toList()
 
 private fun solvePartOne(allMessages: List<String>) {
     val message = (0..allMessages[0].length - 1)
-            .flatMap { i -> getMostCommonCharacterList(allMessages.map { it[i] }.toCharArray()) }
+            .map { i -> getMostCommonCharacter(allMessages.map { it[i] }.toCharArray()) }
             .joinToString("")
 
     println("6.1 - Decoded message is $message")
 }
 
-
 private fun solvePartTwo(allMessages: List<String>) {
     val message = (0..allMessages[0].length - 1)
-            .flatMap { i -> getLeastCommonCharacterList(allMessages.map { it[i] }.toCharArray()) }
+            .map { i -> getLeastCommonCharacter(allMessages.map { it[i] }.toCharArray()) }
             .joinToString("")
 
     println("6.2 - Decoded message is $message")
+}
+
+fun main(args: Array<String>) {
+    val allMessages = InputUtils().getInputAsText("src/day6/input.txt").readText().split("\n")
+
+    solvePartOne(allMessages)
+    solvePartTwo(allMessages)
 }
